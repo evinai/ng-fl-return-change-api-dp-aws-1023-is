3303b1a6ddef6b582583068237eff23b58dd3d7f
evinai <ttunca@evin-ai.com>
2023-10-13 21:16:30 +0000

    updated home route

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def change(amount):
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! I can make change at route: /change'
+    return 'Hello World! I can make change at route plese feed me: /change'
 
 @app.route('/change/<dollar>/<cents>')
 def changeroute(dollar, cents):
